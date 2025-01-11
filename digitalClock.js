15a770b8ed16bb65b8384bb2be48bd5d689e96f0
AnanthaDatta_S <126938357+Ananthadatta02@users.noreply.github.com>
2025-01-11 17:53:29 +0530

    Update digitalClock.js

--- digitalClock.js
+++ digitalClock.js
@@ -1,6 +1,5 @@
-function clock(){
+function clock() {
     var date = new Date();
-    console.log(date);
     var dd = date.getDate();
     var mo = date.getMonth();
     var yy = date.getFullYear();
@@ -8,41 +7,43 @@ function clock(){
     var mm = date.getMinutes();
     var ss = date.getSeconds();
     var dy = date.getDay();
-    var imgArr = ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg"]
-    document.body.style.backgroundImage = `url(${imgArr[dy]})`
-    var fullMon = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
-    var fullDay = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+    var imgArr = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg"];
+    document.body.style.backgroundImage = `url(${imgArr[dy]})`;
+
+    var fullMon = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    var fullDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     mo = fullMon[mo];
     dy = fullDay[dy];
 
-    var am_pm = "AM"
-    if(hh>=12){
-        am_pm = "PM"
-        if(hh>12){
+    var am_pm = "AM";
+    if (hh >= 12) {
+        am_pm = "PM";
+        if (hh > 12) {
             hh -= 12;
         }
     }
-    if(hh==0){
-        hh=12;
+    if (hh == 0) {
+        hh = 12;
     }
-    if(hh<=10){
-        hh = `0${hh}`
+    if (hh < 10) {
+        hh = `0${hh}`;
     }
-    if(mm<=10){
-        mm = `0${mm}`
+    if (mm < 10) {
+        mm = `0${mm}`;
     }
-    if(ss<10){
-        ss = `0${ss}`
+    if (ss < 10) {
+        ss = `0${ss}`;
     }
-    if(dd<10){
-        dd = `0${dd}`
+    if (dd < 10) {
+        dd = `0${dd}`;
     }
 
-    document.getElementById("time").innerHTML = `${hh}:${mm} ${am_pm}`
-    document.getElementById("date").innerHTML = `${dd} - ${mm} - ${yy}`
+    document.getElementById("time").innerHTML = `${hh}:${mm} ${am_pm}`;
+    document.getElementById("date").innerHTML = `${dd} - ${mo} - ${yy}`;
     document.getElementById("day").innerHTML = dy;
     document.getElementById("ss").innerHTML = ss;
 }
-clock();
 
-setInterval(clock,1000);
+clock();
+setInterval(clock, 1000);
